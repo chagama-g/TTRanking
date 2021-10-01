6987dddd701a1b112ab8dca0145c157b90e0ab6b
chagama-g <55901504+chagama-g@users.noreply.github.com>
2021-10-01 22:15:38 +0900

    🎉 Add Tampermonkey meta information

---
+++ ttranking.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         TTRanking
+// @namespace    https://github.com/chagama-g/
+// @version      0.1
+// @description  Typing Tube のランキング表示
+// @author       You
+// @match        https://typing-tube.net/movie/*/*
+// @icon         https://www.google.com/s2/favicons?domain=typing-tube.net
+// @grant        none
+// ==/UserScript==
+
