41afa0b16d5ae37f8385c0cedc97f39b1bf9a33d
chagama-g <55901504+chagama-g@users.noreply.github.com>
2021-10-01 23:42:05 +0900

    Conflict resolution

--- ttranking.user.js
+++ ttranking.user.js
@@ -14,4 +14,37 @@
 
     // Your code here...
 
+    class User {
+        constructor(name, score) {
+            this.name = name;
+            this.score = score;
+        }
+    }
+
+    console.log("TTRanking");
+
+    let users = getRankingList();
+    users.forEach((elem, index) => {
+        console.log(elem.name, elem.score);
+    });
+
+
+    function getRankingList() {
+        let elems = document.getElementsByClassName("list-group-item p-2");
+        let arry = Array.from(elems);
+
+        let users = new Array(arry.length);
+
+        arry.forEach((elem, index) => {
+            let name = elem.getElementsByClassName("player_ranking_name")[0].textContent;
+
+            let score = elem.getElementsByClassName("player_ranking")[0].textContent;
+            score = score.slice(0, score.length - 1);
+
+            users[index] = new User(name, score);
+        });
+
+        return users;
+    }
+
 })();
